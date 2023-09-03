2521c589c22292d547af4f5096fdf6b157d168a1
Chebel405 <anne.chebel@gmail.com>
2023-09-03 11:43:04 +0200

    first commit

---
+++ src/test/java/com/example/morpion/MorpionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.morpion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
